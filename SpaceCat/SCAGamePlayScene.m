18adecf8a65b3d8c834e48ec2aeef05197dab82e
ilyatoka <ilyatoka@yahoo.com>
2015-12-28 01:15:04 +0200

    Add texture atlas

--- SpaceCat/SCAGamePlayScene.m
+++ SpaceCat/SCAGamePlayScene.m
@@ -9,6 +9,11 @@
         SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"gamePlayBackground"];
         background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         [self addChild:background];
+        
+        SKSpriteNode *machine = [SKSpriteNode spriteNodeWithImageNamed:@"machine_1"];
+        machine.position = CGPointMake(CGRectGetMidX(self.frame), 12);
+        machine.anchorPoint = CGPointMake(0.5, 0);
+        [self addChild:machine];
     }
     
     return self;
